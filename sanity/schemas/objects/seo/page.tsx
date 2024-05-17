0fcd88121185bfb4ef8e020d693562f5da8470d9
Briana Gude <briana.gude@gmail.com>
2024-05-17 17:54:41 -0400

    fix error

--- sanity/schemas/objects/seo/page.tsx
+++ sanity/schemas/objects/seo/page.tsx
@@ -19,7 +19,6 @@ export default defineField({
           If empty, displays the document title (<code>title</code>)
         </>
       ),
-      options: {field: 'title'},
       validation: (Rule) =>
         Rule.max(50).warning('Longer titles may be truncated by search engines'),
     }),
