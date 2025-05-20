e2f1a9593a63317482b6754834a008d6b44bbced
Briana Gude <briana.gude@gmail.com>
2025-05-20 10:00:18 -0400

    install GA

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { FontITC } from '@/fonts/FontUtils'
 import "@/styles/main.scss"
 import { fetchMetadata } from '@/components/FetchMetadata'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 // META STUFF
 export const metadata: Metadata = await fetchMetadata();
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${FontITC.variable}`}>
        {children}
+       <GoogleAnalytics gaId="G-Q0MQPRB730" />
       </body>
     </html>
   )
