2176d1b41659a81cb3a6b483d694bec9bad250a3
Briana Gude <briana.gude@gmail.com>
2025-09-13 22:43:49 -0400

    update ga tags

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { FontITC } from '@/fonts/FontUtils'
 import "@/styles/main.scss"
 import { fetchMetadata } from '@/components/FetchMetadata'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 
 // META STUFF
 export const metadata: Metadata = await fetchMetadata();
@@ -18,6 +18,7 @@ export default function RootLayout({
       <body className={`${FontITC.variable}`}>
        {children}
        <GoogleAnalytics gaId="G-Q0MQPRB730" />
+       <GoogleTagManager gtmId="G-Q0MQPRB730" />
       </body>
     </html>
   )
